3953c9ef4e6cdd6e78dd88a8864b7459579c71ca
MEAlzaimer <projetointegradorfacamsd@gmail.com>
2018-09-07 12:27:39 -0300

    SIDNEY-SPLASH SCREEN

--- app/src/main/java/mealzaimerproject/com/mealzaimer/MainActivity.java
+++ app/src/main/java/mealzaimerproject/com/mealzaimer/MainActivity.java
@@ -1,13 +1,26 @@
 package mealzaimerproject.com.mealzaimer;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    private ImageButton botaoContinuar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        botaoContinuar=(ImageButton) findViewById(R.id.btnContinuar);
+        botaoContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MenuInicialActivity.class));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/mealzaimerproject/com/mealzaimer/SplashActivity.java
@@ -0,0 +1,28 @@
+package mealzaimerproject.com.mealzaimer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //colocando pra abrir em full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //metodo que define o tempo da slapsh
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            //abrir a mmain activyty
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        },4000);
+    }
+}
